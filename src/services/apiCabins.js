ac9b896e1c80654896bd0b9d903ab3e48b08d648
Adelina-hub <adelina.macarascu@gmail.com>
2025-06-09 22:27:42 +0400

    supabase

---
+++ src/services/apiCabins.js
@@ -0,0 +1,11 @@
+import { supabase } from "./supabase";
+
+export async function getCabins() {
+  const { data: cabins, error } = await supabase.from("cabins").select("*");
+  if (error) {
+    console.error(error);
+    throw new Error("Error fetchung the cabins");
+  }
+
+  return data;
+}
---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://emhqanjouuhtbhxloptm.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVtaHFhbmpvdXVodGJoeGxvcHRtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk0Nzg0OTIsImV4cCI6MjA2NTA1NDQ5Mn0.o8ogxa_Puz6sur1lBvr6h_iKlBgeJjf8rXoEhdyOcaY";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
