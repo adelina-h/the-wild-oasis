da3aa1dac8955fdda17196f2cdbf81d8d06628f5
Adelina-hub <adelina.macarascu@gmail.com>
2025-06-10 13:54:41 +0400

    buckets

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,7 +1,7 @@
-import { supabase } from "./supabase";
+import supabase from "./supabase";
 
 export async function getCabins() {
-  const { data: cabins, error } = await supabase.from("cabins").select("*");
+  const { data, error } = await supabase.from("cabins").select("*");
   if (error) {
     console.error(error);
     throw new Error("Error fetchung the cabins");
