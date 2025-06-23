d520bc00e3be19e0f13363bbbf3c42c61d4fa24d
Adelina-hub <adelina.macarascu@gmail.com>
2025-06-23 20:18:31 +0400

    upd setting

--- src/features/settings/useSettings.js
+++ src/features/settings/useSettings.js
@@ -1,1 +1,14 @@
-export function useSettings() {}
+import { useQuery } from "@tanstack/react-query";
+import { getSettings } from "../../services/apiSettings";
+
+export function useSettings() {
+  const {
+    isLoading,
+    error,
+    data: settings,
+  } = useQuery({
+    queryKey: ["settings"],
+    queryFn: getSettings,
+  });
+  return { isLoading, error, settings };
+}
