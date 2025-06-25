27bf2df7613b946ba9d10bfe671f2f1ae4109992
Adelina-hub <adelina.macarascu@gmail.com>
2025-06-25 19:27:14 +0400

    added the modal

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,5 +1,11 @@
 import styled from "styled-components";
-
+import PropTypes from "prop-types";
+import { HiXMark } from "react-icons/hi2";
+import { createPortal } from "react-dom";
+Modal.propTypes = {
+  children: PropTypes.node.isRequired,
+  onClose: PropTypes.func.isRequired,
+};
 const StyledModal = styled.div`
   position: fixed;
   top: 50%;
@@ -44,7 +50,24 @@ const Button = styled.button`
     height: 2.4rem;
     /* Sometimes we need both */
     /* fill: var(--color-grey-500);
-    stroke: var(--color-grey-500); */
+  //     stroke: var(--color-grey-500); 
+     color: var(--color-grey-500);   */
     color: var(--color-grey-500);
   }
 `;
+
+function Modal({ children, onClose }) {
+  return createPortal(
+    <Overlay>
+      <StyledModal>
+        <Button onClick={onClose}>
+          <HiXMark />
+        </Button>
+        <div>{children}</div>
+      </StyledModal>
+    </Overlay>,
+    document.body
+  );
+}
+
+export default Modal;
