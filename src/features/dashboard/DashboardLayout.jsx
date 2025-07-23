aba9ce97abcf67db968e89897895f814e631bc69
Adelina-hub <adelina.macarascu@gmail.com>
2025-07-23 16:16:09 +0300

    charts

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -4,6 +4,7 @@ import Spinner from "../../ui/Spinner";
 import { useRecentStays } from "./useRecentStays";
 import Stats from "./Stats";
 import { useCabins } from "../cabins/useCabins";
+import SalesChart from "./SalesChart";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -33,6 +34,7 @@ function DashboardLayout() {
         cabinCount={cabins.length}
       />
       <div>PLACEHOLDER DASHBOARD CONTENT</div>
+      <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   );
 }
