c87e0145c3d0101f4132008728703d64c884a599
Adelina-hub <adelina.macarascu@gmail.com>
2025-07-24 15:48:19 +0300

    pie chart

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -5,6 +5,7 @@ import { useRecentStays } from "./useRecentStays";
 import Stats from "./Stats";
 import { useCabins } from "../cabins/useCabins";
 import SalesChart from "./SalesChart";
+import DurationChart from "./DurationChart";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -34,6 +35,7 @@ function DashboardLayout() {
         cabinCount={cabins.length}
       />
       <div>PLACEHOLDER DASHBOARD CONTENT</div>
+      <DurationChart confirmedStays={confirmedStays} />
       <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   );
