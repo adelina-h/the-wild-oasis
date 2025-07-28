8dbe220030d676c86784fc335f06195519e1ed3f
Adelina-hub <adelina.macarascu@gmail.com>
2025-07-28 14:31:07 +0300

    today activity

--- src/features/check-in-out/TodayItem.jsx
+++ src/features/check-in-out/TodayItem.jsx
@@ -1,4 +1,24 @@
 import styled from "styled-components";
+import PropTypes from "prop-types";
+
+import Tag from "../../ui/Tag";
+import { Flag } from "../../ui/Flag";
+import Button from "../../ui/Button";
+import { Link } from "react-router-dom";
+import CheckoutButton from "./CheckoutButton";
+
+TodayItem.propTypes = {
+  activity: PropTypes.shape({
+    id: PropTypes.any,
+    status: PropTypes.oneOf(["unconfirmed", "checked-in"]).isRequired,
+    guests: PropTypes.shape({
+      countryFlag: PropTypes.string.isRequired,
+      country: PropTypes.string.isRequired,
+      fullName: PropTypes.string.isRequired,
+    }).isRequired,
+    numNights: PropTypes.number.isRequired,
+  }).isRequired,
+};
 
 const StyledTodayItem = styled.li`
   display: grid;
@@ -18,3 +38,28 @@ const StyledTodayItem = styled.li`
 const Guest = styled.div`
   font-weight: 500;
 `;
+
+function TodayItem({ activity }) {
+  const { id, status, guests, numNights } = activity;
+  return (
+    <StyledTodayItem>
+      {status === "unconfirmed" && <Tag type="green">Ariving</Tag>}
+      {status === "checked-in" && <Tag type="blue">Departing</Tag>}
+      <Flag src={guests.countryFlag} alt={`Flag of ${guests.country}`} />
+      <Guest>{guests.fullName}</Guest>
+      <div>{numNights} nights</div>
+      {status === "unconfirmed" && (
+        <Button
+          size="small"
+          variation="primary"
+          as={Link}
+          to={`/checkin/${id}`}
+        >
+          Check in
+        </Button>
+      )}
+      {status === "checked-in" && <CheckoutButton bookingId={id} />}
+    </StyledTodayItem>
+  );
+}
+export default TodayItem;
